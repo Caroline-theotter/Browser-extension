c45ff1a83cbe4d054dd18ec0b27f47b85de4edbe
Zineb-Ada <85489706+Zineb-Ada@users.noreply.github.com>
2021-09-14 15:23:42 +0200

    with switch button

---
+++ projet-extentions-web/Extension-Ecolo/popup-ecolo.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', ()=> {
+    var checkbox = document.querySelector('input[type="checkbox"]'); 
+     checkbox.addEventListener('change', ()=> {
+
+      if (checkbox.checked) {
+        
+      }
+   });
+  });
